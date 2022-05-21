57b89dce928fd60984003e0b14e21230508e579f
Hozefa Mangalorewala <hozefam@live.in>
2022-05-21 13:22:40 +0300

    Added Play.Inventory Service

---
+++ Play.Inventory/src/Play.Inventory.Service/Entities/InventoryItem.cs
@@ -0,0 +1,14 @@
+using System;
+using Play.Common.Entities;
+
+namespace Play.Inventory.Service.Entites
+{
+    public class InventoryItem : IEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CatalogItemId { get; set; }
+        public int Quantity { get; set; }
+        public DateTimeOffset AcquiredDate { get; set; }
+    }
+}
